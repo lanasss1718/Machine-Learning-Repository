70bb78534c8ca35f64858191d0cccba995ee525b
lanas <133148831+lanasss1718@users.noreply.github.com>
2023-05-11 16:45:19 +0300

    Update MachineLearning.py
    This is just a basic example, but you could include more advanced algorithms like decision trees, k-nearest neighbors, or support vector machines. Don't forget to include documentation and comments to help other users understand the code!

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,30 +1,20 @@
-import numpy as np
+# import necessary libraries
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 
-# Load data
+# read in data and split into training and testing sets
 data = pd.read_csv('data.csv')
+X = data.drop('target_variable', axis=1)
+y = data['target_variable']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Split data into train and test sets
-train_data = data.sample(frac=0.8, random_state=42)
-test_data = data.drop(train_data.index)
-
-# Prepare data for training
-train_X = train_data.drop(['target'], axis=1).values
-train_y = train_data['target'].values
-
-# Initialize linear regression model
+# fit linear regression model
 model = LinearRegression()
+model.fit(X_train, y_train)
 
-# Train model
-model.fit(train_X, train_y)
-
-# Predict using trained model
-test_X = test_data.drop(['target'], axis=1).values
-test_y = test_data['target'].values
-pred_y = model.predict(test_X)
-
-# Evaluate model using mean squared error
-mse = mean_squared_error(test_y, pred_y)
-print('Mean squared error: ', mse)
+# evaluate model performance
+train_score = model.score(X_train, y_train)
+test_score = model.score(X_test, y_test)
+print("Train score: ", train_score)
+print("Test score: ", test_score)
